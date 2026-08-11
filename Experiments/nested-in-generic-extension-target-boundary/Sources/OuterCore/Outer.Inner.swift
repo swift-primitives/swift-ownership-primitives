d90ccbbcf482adadd3572a1dd0b78aaddedcd403
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-11 15:03:56 +0200

    Remove migrated Research and Experiments corpus (#9)

--- Experiments/nested-in-generic-extension-target-boundary/Sources/OuterCore/Outer.Inner.swift
+++
@@ -1,3 +0,0 @@
-extension Outer {
-    public enum Inner {}
-}
--- Experiments/nested-in-generic-extension-target-boundary/Sources/Umbrella/exports.swift
+++
@@ -1,3 +0,0 @@
-@_exported public import OuterNamespace
-@_exported public import OuterCore
-@_exported public import VariantPrimitives
